3fe05752612c7e123ee22c88f0be77d84b482028
carmar04 <2dam.csoro@institutoserpis.org>
2018-09-14 10:43:54 +0200

    Añadido CCategoria en Gtk

---
+++ CCategoria/CCategoria/MainWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+		treeview1.AppendColumn("ID", new CellRendererText(), "text", 0);
+		treeview1.AppendColumn("Nombre", new CellRendererText(), "text", 1);
+
+		ListStore listStore = new ListStore(typeof(string), typeof(string));
+		treeview1.Model = listStore;
+		listStore.AppendValues("1", "cat 1");
+		listStore.AppendValues("2", "cat 2");
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
--- CMySql/CMySql/Program.cs
+++ CMySql/CMySql/Program.cs
@@ -1,21 +1,39 @@
 ﻿using System;
 using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace CMySql
 {
     class MainClass
     {
+		private static string[] getFieldNames(IDataReader dataReader){
+			int aux = dataReader.FieldCount;
+			for (int i = 0; i < aux; i++)
+            {
+				dataReader.GetName(int i);
+				string cadena += ;
+            }
+		}
         public static void Main(string[] args)
         {
-			MySqlConnection mySqlConnection = new MySqlConnection(
+			IDbConnection dbConnection = new MySqlConnection(
 				"server=localhost;" +
 				"database=dbprueba;" +
 				"user=root;" +
 				"password=sistemas;" +
 				"ssl-mode=none;"
 			);
-			mySqlConnection.Open();
-			mySqlConnection.Close();
+			dbConnection.Open();
+			IDbCommand dbCommand = dbConnection.CreateCommand();
+			dbCommand.CommandText = "select * from categoria order by id";
+			IDataReader dataReader = dbCommand.ExecuteReader();
+			dataReader.get
+			while(dataReader.Read()){
+				Console.WriteLine("id={0} nombre={1}", dataReader["id"], dataReader["nombre"]);
+			}
+			dataReader.Close();
+
+			dbConnection.Close();
         }
     }
 }
