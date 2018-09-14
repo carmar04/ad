a07b16962c0e0be013c12e76a71fc07c285684a9
carmar04 <2dam.csoro@institutoserpis.org>
2018-09-14 09:19:39 +0200

    Añadidos MySql.Data.dll CHolaMundo y CMySql

---
+++ CMySql/CMySql/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace CMySql
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			MySqlConnection mySqlConnection = new MySqlConnection(
+				"server=localhost;" +
+				"database=dbprueba;" +
+				"user=root;" +
+				"password=sistemas;" +
+				"ssl-mode=none;"
+			);
+			mySqlConnection.Open();
+			mySqlConnection.Close();
+        }
+    }
+}
