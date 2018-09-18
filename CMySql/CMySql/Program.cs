d4585a7442f40ff1d7204d2c22684bfaee72d8b8
carmar04 <2dam.csoro@institutoserpis.org>
2018-09-18 09:48:32 +0200

    Añadido revisión MySql

--- CMySql/CMySql/Program.cs
+++ CMySql/CMySql/Program.cs
@@ -1,18 +1,26 @@
 ﻿using System;
 using MySql.Data.MySqlClient;
 using System.Data;
+using System.Collections.Generic;
 
 namespace CMySql
 {
     class MainClass
     {
 		private static string[] getFieldNames(IDataReader dataReader){
-			int aux = dataReader.FieldCount;
-			for (int i = 0; i < aux; i++)
-            {
-				dataReader.GetName(int i);
-				string cadena += ;
-            }
+			//int fieldCount = dataReader.FieldCount;
+			//string [] fieldNames = new string[fieldCount];
+			//for (int index = 0; index < fieldCount; index++)
+			//{
+			//fieldNames.SetValue(dataReader.GetName(index), index);
+			//}
+			//return fieldNames;
+			List<string> fieldNamelist = new List<string>();
+			int fieldCount = dataReader.FieldCount;
+			for (int index = 0; index < dataReader.FieldCount;index++){
+				fieldNamelist.Add(dataReader.GetName(index));
+			}
+			return fieldNamelist.ToArray();
 		}
         public static void Main(string[] args)
         {
@@ -25,9 +33,15 @@ namespace CMySql
 			);
 			dbConnection.Open();
 			IDbCommand dbCommand = dbConnection.CreateCommand();
-			dbCommand.CommandText = "select * from categoria order by id";
+			dbCommand.CommandText = "select id, nombre from categoria order by id";
 			IDataReader dataReader = dbCommand.ExecuteReader();
-			dataReader.get
+			Console.WriteLine("Numero de columnas= "+dataReader.FieldCount);
+
+			for (int index = 0; index < dataReader.FieldCount;index++){
+//				Console.WriteLine("Columna {0} = {1}", index, dataReader.GetName(index));
+				Console.Write("Columna:"+getFieldNames(dataReader)[index]+" ");
+			}
+			Console.WriteLine();
 			while(dataReader.Read()){
 				Console.WriteLine("id={0} nombre={1}", dataReader["id"], dataReader["nombre"]);
 			}
