907dbdb60014a7df101983059ee109381fb6ba0a
carmar04 <2dam.csoro@institutoserpis.org>
2018-11-23 10:50:02 +0100

    Añadido singleton

---
+++ GMySql/src/main/java/serpis/ad/App.java
@@ -0,0 +1,25 @@
+package serpis.ad;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class App {
+	
+	private static App instance = new App();
+	
+	public static App getInstance() {
+		return instance;
+	}
+	
+	private Connection connection;
+	
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+	public Connection getConnection() {
+		return connection;
+	}
+}
+		
+
--- GMySql/src/main/java/serpis/ad/Prueba.java
+++ GMySql/src/main/java/serpis/ad/Prueba.java
@@ -2,24 +2,43 @@ package serpis.ad;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDateTime;
 
 
 public class Prueba {
 
 	public static void main(String[] args) throws SQLException {
-		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/dbprueba", "root", "sistemas");
+		
+		String connectionSql = "jdbc:mysql://localhost/dbprueba?user=root&password=sistemas";
+		
+		App.getInstance().setConnection(DriverManager.getConnection(connectionSql));
+						
+ 		App.getInstance().getConnection();
+		
 		//Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/dbprueba?user=root&password=sistemas");
-		Statement statement = (Statement) connection.createStatement();
+		Statement statement = App.getInstance().getConnection().createStatement();
+		insert();
 		ResultSet resultSet = statement.executeQuery("select * from categoria");
 		
 		while(resultSet.next()) {
 			System.out.printf("%s %s\n", resultSet.getObject(1), resultSet.getObject(2));
 		}
 		statement.close();
-		connection.close();
+		App.getInstance().getConnection().close();
+		
 	}
-
+	private static void insert() throws SQLException {
+		String insertSql = "insert into categoria (nombre) values (?)";
+		PreparedStatement preparedStatement = App.getInstance().getConnection().prepareStatement(insertSql);
+		preparedStatement.setObject(1, "categoria " + LocalDateTime.now());
+		preparedStatement.executeUpdate();
+		preparedStatement.close();
+	}
+	private static void update() throws SQLException {
+		String updateSql = "update categoria set (nombre)"
+	}
 }
